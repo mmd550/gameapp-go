bd84456691e8a290f52af255cc930d30a4e41e62
Mohamad Farvardin <mmd550@taline.ir>
2026-06-12 21:21:51 +0330

    refactor: Remove extra code

--- pkg/phonenumber/phonenumber.go
+++
@@ -1,9 +0,0 @@
-package phonenumber
-
-import "regexp"
-
-var iranianMobileRegex = regexp.MustCompile(`^(\+98|0)?9\d{9}$`)
-
-func IsValid(number string) bool {
-	return iranianMobileRegex.MatchString(number)
-}
